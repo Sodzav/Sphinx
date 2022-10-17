f088c42fc1ff3a802c28c1cf1ba07b45f17a9546
Sodzav <danjrus28@gmail.com>
2022-10-17 22:38:23 +0500

    2rst commit

---
+++ build/html/_sources/index.rst.txt
@@ -0,0 +1,18 @@
+.. Pars documentation master file, created by
+   sphinx-quickstart on Tue Oct 11 12:51:17 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Добро пожаловать в документацию Pars
+====================================
+
+.. toctree::
+   example1
+   example2
+   example3
+   example4
+   example5
+   :maxdepth: 3
+
+
+
---
+++ source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Pars'
+copyright = '2022, Oborin Daniil'
+author = 'Oborin Daniil'
+release = '\x1b[F\x1b[B\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[C\x1b[D\x1b[D\x1b[D\x1b[D\x1b[D1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'ru'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
